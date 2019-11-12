ce55bed0ceeb4d353925f7525f49a8cca946b8d0
Bil Moorhead <bil@alitheon.com>
2019-11-12 11:40:37 -0800

    updated to use a 'Universal' image

--- Sources/Multipart/UIKit.swift
+++ Sources/Multipart/UIKit.swift
@@ -5,13 +5,40 @@
 //  Created by Bil Moorhead on 10/16/19.
 //
 
-#if os(iOS)
+#if os(macOS)
+
+import AppKit
+public typealias UniversalImage = NSImage
+
+extension NSBitmapImageRep {
+	
+    public func png() ->Data? { representation(using: .png, properties: [:]) }
+	public func jpeg(_ compressionQuality: CGFloat) ->Data? { representation(using: .jpeg, properties: [.compressionFactor: compressionQuality]) }
+	
+}
+extension Data {
+	
+    public var bitmap: NSBitmapImageRep? { NSBitmapImageRep(data: self) }
+	
+}
+extension NSImage {
+	
+    public func pngData() ->Data? { tiffRepresentation?.bitmap?.png() }
+    public func jpegData(compressionQuality: CGFloat) ->Data? { tiffRepresentation?.bitmap?.jpeg(compressionQuality) }
+	
+}
+
+#else
+
 import UIKit
+public typealias UniversalImage = UIImage
+
+#endif
 
 extension MultiPart.Part {
 
 	public static func png(
-		_ image: UIImage,
+		_ image: UniversalImage,
 		named name:String,
 		filename: String? = nil
 	) ->MultiPart.Part {
@@ -22,7 +49,7 @@ extension MultiPart.Part {
 	}
 	
 	public static func jpg(
-		_ image: UIImage,
+		_ image: UniversalImage,
 		compression:CGFloat = 1.0,
 		named name:String,
 		filename: String? = nil
@@ -34,5 +61,3 @@ extension MultiPart.Part {
 	}
 	
 }
-
-#endif
